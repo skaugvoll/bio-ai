42d76fae08fba31d655c5cf99633d395595bb8d0
Sigve Skaugvoll <skaugvoll@gmail.com>
2018-01-28 14:35:08 +0100

    huff

---
+++ mvdpr/src/main/java/mdvpr/DataGenerator.java
@@ -0,0 +1,42 @@
+package mdvpr;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class DataGenerator {
+
+    public String dataPath;
+    public String solutionpath;
+
+    public DataGenerator(String filename){
+
+        this.dataPath = "/DataFiles/" + filename;
+        InputStream is = DataGenerator.class.getResourceAsStream(this.dataPath);
+        try{
+            this.readfile(is);
+        }
+        catch (Exception e){
+            System.out.println("Fåkk off");
+        }
+
+
+    }
+
+    private void readfile(InputStream path){
+        Scanner scanner = new Scanner(path);
+        while(scanner.hasNextLine()){
+            System.out.println(scanner.nextLine());
+        }
+        scanner.close();
+    }
+
+    public static void main(String[] args) {
+        DataGenerator dg = new DataGenerator("p01");
+    }
+
+
+
+
+
+}
---
+++ mvdpr/src/main/java/mdvpr/Main.java
@@ -0,0 +1,10 @@
+package mdvpr;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello thomas");
+    }
+
+}
