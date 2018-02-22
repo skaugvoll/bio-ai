084fc99e2a3e0dd910c5c7346744bcbdd02087a8
Sigve Skaugvoll <skaugvoll@gmail.com>
2018-02-22 10:39:55 +0100

    now converts bgr value to rgb

--- moea/src/main/java/moea/DataGenerator.java
+++ moea/src/main/java/moea/DataGenerator.java
@@ -4,25 +4,43 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
 import java.io.IOException;
-import java.util.Arrays;
+
 
 
 public class DataGenerator {
 
     public void readImage(String number) {
-        String resourcePath = "/TestImages/1/Test image.jpg";
+        String resourcePath = "/TestImages/"+number+"/Test image.jpg";
 
         try{
             BufferedImage hugeImage = ImageIO.read(DataGenerator.class.getResourceAsStream(resourcePath));
             System.out.println("okay, now bufferdImage");
+
             int[][] result = convertTo2DWithoutUsingGetRGB(hugeImage);
-            System.out.println(Arrays.toString(result));
+//            int[][] result = convertTo2DUsingGetRGB(hugeImage);
+
+            System.out.println(this.GBRtoRGB(result[0][0]));
         }
         catch (IOException e){
             System.out.println("fåkk, something went wrong!\n" + e);
         }
 
 
+    }
+
+    private static int[][] convertTo2DUsingGetRGB(BufferedImage image) {
+        int width = image.getWidth();
+        int height = image.getHeight();
+        int[][] result = new int[height][width];
+
+        for (int row = 0; row < height; row++) {
+            for (int col = 0; col < width; col++) {
+                result[row][col] = image.getRGB(col, row);
+                System.out.println(image.getRGB(col, row));
+            }
+        }
+
+        return result;
     }
 
 
@@ -32,6 +50,7 @@ public class DataGenerator {
         final int width = image.getWidth();
         final int height = image.getHeight();
         final boolean hasAlphaChannel = image.getAlphaRaster() != null;
+        System.out.println("hasAlphaCHannel: " + hasAlphaChannel);
 
         int[][] result = new int[height][width];
         if (hasAlphaChannel) {
@@ -67,6 +86,20 @@ public class DataGenerator {
         }
 
         return result;
+    }
+
+
+    public int[] GBRtoRGB(int pixelBGRinteger){
+        int  red   = (pixelBGRinteger & 0x00ff0000) >> 16;
+        int  green = (pixelBGRinteger & 0x0000ff00) >> 8;
+        int  blue  =  pixelBGRinteger & 0x000000ff;
+        System.out.println("Red Color value = "+ red);
+        System.out.println("Green Color value = "+ green);
+        System.out.println("Blue Color value = "+ blue);
+        int[] data = {red,green,blue};
+        return data;
+
+
     }
 
 
