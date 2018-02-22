caa1b12db296618846bec6fc1a3d11dd6609a4af
Sigve Skaugvoll <skaugvoll@gmail.com>
2018-02-22 09:26:29 +0100

    jau startet å tenke på lesing av bilder

---
+++ moea/src/main/java/moea/DataGenerator.java
@@ -0,0 +1,79 @@
+package moea;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.io.IOException;
+import java.util.Arrays;
+
+
+public class DataGenerator {
+
+    public void readImage(String number) {
+        String resourcePath = "/TestImages/1/Test image.jpg";
+
+        try{
+            BufferedImage hugeImage = ImageIO.read(DataGenerator.class.getResourceAsStream(resourcePath));
+            System.out.println("okay, now bufferdImage");
+            int[][] result = convertTo2DWithoutUsingGetRGB(hugeImage);
+            System.out.println(Arrays.toString(result));
+        }
+        catch (IOException e){
+            System.out.println("fåkk, something went wrong!\n" + e);
+        }
+
+
+    }
+
+
+    private static int[][] convertTo2DWithoutUsingGetRGB(BufferedImage image) {
+
+        final byte[] pixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+        final int width = image.getWidth();
+        final int height = image.getHeight();
+        final boolean hasAlphaChannel = image.getAlphaRaster() != null;
+
+        int[][] result = new int[height][width];
+        if (hasAlphaChannel) {
+            final int pixelLength = 4;
+            for (int pixel = 0, row = 0, col = 0; pixel < pixels.length; pixel += pixelLength) {
+                int argb = 0;
+                argb += (((int) pixels[pixel] & 0xff) << 24); // alpha
+                argb += ((int) pixels[pixel + 1] & 0xff); // blue
+                argb += (((int) pixels[pixel + 2] & 0xff) << 8); // green
+                argb += (((int) pixels[pixel + 3] & 0xff) << 16); // red
+                result[row][col] = argb;
+                col++;
+                if (col == width) {
+                    col = 0;
+                    row++;
+                }
+            }
+        } else {
+            final int pixelLength = 3;
+            for (int pixel = 0, row = 0, col = 0; pixel < pixels.length; pixel += pixelLength) {
+                int argb = 0;
+                argb += -16777216; // 255 alpha
+                argb += ((int) pixels[pixel] & 0xff); // blue
+                argb += (((int) pixels[pixel + 1] & 0xff) << 8); // green
+                argb += (((int) pixels[pixel + 2] & 0xff) << 16); // red
+                result[row][col] = argb;
+                col++;
+                if (col == width) {
+                    col = 0;
+                    row++;
+                }
+            }
+        }
+
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        DataGenerator dg = new DataGenerator();
+        dg.readImage("1");
+    }
+
+
+}
