c03edece01e57585cb0064d63775d8b1172236d4
Sigve Skaugvoll <skaugvoll@gmail.com>
2018-04-23 11:45:31 +0200

    init assignment 3

---
+++ jssp/src/main/java/jssp/Main.java
@@ -0,0 +1,13 @@
+package jssp;
+
+public class Main {
+
+    public static void greetings(){
+        System.out.println("Hello jssp, lets get you solved");
+    }
+
+    public static void main(String[] args) {
+        Main.greetings();
+    }
+
+}
