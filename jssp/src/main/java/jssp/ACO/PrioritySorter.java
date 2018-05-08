2675964e7e29c943d44e92b913413f5ca6b42fe8
Sigve Skaugvoll <skaugvoll@gmail.com>
2018-05-08 16:08:17 +0200

    trying to remove merge conflict

--- jssp/src/main/java/jssp/ACO/PrioritySorter.java
+++
@@ -1,9 +0,0 @@
-package jssp.ACO;
-
-import java.util.Comparator;
-
-public class PrioritySorter implements Comparator<Pheromone> {
-    public int compare(Pheromone o1, Pheromone o2) {
-        return Double.compare(o1.getPriority(), o2.getPriority());
-    }
-}
